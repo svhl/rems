3c8bd011eb38836f6360d3625e20216b11593071
Deril Jose <90505282+deriljose@users.noreply.github.com>
2025-02-01 12:43:12 +0530

    Update server.js
    for database connection

--- server.js
+++ server.js
@@ -6,21 +6,21 @@ const port = 3000;
 const multer = require("multer");
 const upload = multer(); // You can configure multer here as needed
 
-// Create MySQL connection
 const connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "helloworld",
-    database: "rajagiri",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
 });
 
 connection.connect((err) => {
-    if (err) {
-        console.error("Error connecting to the database:", err.stack);
-        return;
-    }
-    console.log("Connected to the database.");
+  if (err) {
+    console.error('Error connecting to the database:', err.stack);
+    return;
+  }
+  console.log('Connected to the database');
 });
+
 
 // Middleware to parse JSON bodies
 app.use(express.json());
