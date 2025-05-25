3eec5bb407ce11c8e4a6bee5d2471b7b50397d1b
juanpereiira <juanjudep@gmail.com>
2025-05-25 10:01:11 +0400

    c

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const upload = multer();
 const connection = mysql.createConnection({
 	host: "localhost",
 	user: "root",
-	password: "password",
+	password: "24June2004",
 	database: "rajagiri",
 });
 
